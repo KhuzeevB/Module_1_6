13e876e630efc50e8c67c73ce53cfa25c7a58bf4
KhuzeevB <b.khouzeev@gmail.com>
2024-11-11 01:50:38 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,10 @@
+all_homework=12
+all_times=1.5
+course_name="Python"
+middle_time=all_times/all_homework
+print("Курс:",course_name,", всего задач:",all_homework,",затрачено часов:",all_times,",среднее время выполнения",middle_time,"часа.")
+
+
+
+
+
